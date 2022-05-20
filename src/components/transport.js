79fc1a9898a5b337abe1e22b9cb41bbfaedb163e
Doca123 <marko.mladenovic1@student.um.si>
2022-05-20 15:09:54 +0200

    React router nastavitev

---
+++ src/components/transport.js
@@ -0,0 +1,10 @@
+const Transport = () => {
+    return (
+        <div><h2 className="subtitle">Average emissions of CO2 in Slovenia in trasport per year</h2>
+        <p className="info">CO2 emissions in average in Slovenia is -- 5 033 642 tons.</p>
+        </div>
+    )
+    
+};
+  
+  export default Transport;
