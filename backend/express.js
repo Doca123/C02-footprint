6f0f6858dec42a75140c724bf31bfc9e7b7e8b0e
Doca123 <marko.mladenovic1@student.um.si>
2022-06-02 17:45:11 +0200

    post i get metoda

--- backend/express.js
+++ backend/express.js
@@ -1,46 +1,46 @@
-const express =require('express')
-const app =express()
+const express = require("express");
+const app = express();
+const mysql = require('mysql2');
+const cors = require("cors");
 
-const mysql = require('mysql')
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'markodoca1',
-    database: 'praktikum',
+  user: "root",
+  host: "localhost",
+  password: "markodoca1",
+  database: "praktikum",
 });
 
+app.post("/create", (req, res) => {
+ 
+  const leto = req.body.leto;
+  const proizvodnja = req.body.proizvodnja;
 
-app.post('/stats',(req,res)=>{
-
-    const name = req.body.name
-    const year = req.body.year
-
-    db.query(
-        "INSERT INTO stats (name, year) VALUES(?,?)",
-    [name,yaer], (err,result)=>{
-        if (err){
-            console.log(err)
-        } else{
-           res.send("Values inserted") 
-        }
-
-    });
-
+  db.query(
+    "INSERT INTO elektricna_energija_vo_slovenija_letno (leto, proizvodnja) VALUES (?,?)",
+    [leto, proizvodnja],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
 });
 
-app.get('/stats', (req,res)=>{
-    
-    db.query("SELECT * FROM elektricna_energija_vo_slovenija_letno", (err,result)=>{
-        if (err){
-            console.log(err)
-        }else{
-            res.send(result)
-        }
-    });
+app.get("/stats", (req, res) => {
+  db.query("SELECT * FROM elektricna_energija_vo_slovenija_letno", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-app.listen(3001, ()=>{
-
-    console.log("running on port 3001 ");
+app.listen(3001, () => {
+  console.log("Your server is running on port 3001");
 });
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,13 +1,83 @@
-const Stats = () => {
-    return (
-        <div>
-           <h1>SHOW DATA</h1>
-           <br />
-           <br />
-            <button>Show data</button>
-        </div>
-    )
-    
-};
+import { useState } from "react";
+import Axios from "axios";
+
+// employeeList = energijaList  setEmployeeList = setEnergijaList
+//addEmployee = addEnergija getEmployee = getEnergija
+
+
+function Stats() {
   
-  export default Stats;
+  const [leto, setLeto] = useState(0);
+  const [proizvodnja, setProizvodnja] = useState(0);
+
+  const [energijaList, setEnergijaList] = useState([]);
+
+  const addEnergija = () => {
+    Axios.post("http://localhost:3001/create", {
+      leto: leto,
+      proizvodnja: proizvodnja,
+    }).then(() => {
+        setEnergijaList([
+        ...energijaList,
+        {
+          
+          leto: leto,
+          proizvodnja: proizvodnja,
+        },
+      ]);
+    });
+  };
+
+  const getEnergija = () => {
+    Axios.get("http://localhost:3001/stats").then((response) => {
+        setEnergijaList(response.data);
+    });
+  };
+
+
+  return (
+    <div className="App">
+      <div className="information">
+        
+        <label>Leto:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setLeto(event.target.value);
+          }}
+        />
+        <label>Proizvodnja (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setProizvodnja(event.target.value);
+          }}
+        />
+        <button onClick={addEnergija}>Dodaj</button>
+      </div>
+      <div className="energija">
+        <button onClick={getEnergija}>Elektricna energija</button>
+
+        {energijaList.map((val, key) => {
+          return (
+            <div className="energiaj1">
+              <div>
+              
+                <h3>leto: {val.leto}</h3>
+              
+                <h3>proizvodnja: {val.proizvodnja}</h3>
+              </div>
+              <div>
+                
+
+               
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Stats;
