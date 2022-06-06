62a247f1b1f1e7b734a197c5a3ba48f528ac38c0
viktornankovski <viktor.nankovski@student.um.si>
2022-06-06 12:45:31 +0200

    first chart data update

--- src/Data.js
+++ src/Data.js
@@ -27,4 +27,123 @@ export const DelezEnergijeZaOgrajevanjeProstorov = [
     centralno_ogrevanje: 81.7,
     daljinsko_ogrevanje: 8.7
 },
+{
+    id: 5,
+    leto: 2004,
+    lokalno_ogrevanje: 9.7,
+    centralno_ogrevanje: 80.8,
+    daljinsko_ogrevanje: 9.6
+},
+{
+    id: 6,
+    leto: 2005,
+    lokalno_ogrevanje: 9.5,
+    centralno_ogrevanje: 81.2,
+    daljinsko_ogrevanje: 9.2
+},
+{
+    id: 7,
+    leto: 2006,
+    lokalno_ogrevanje: 11,
+    centralno_ogrevanje: 79.8,
+    daljinsko_ogrevanje: 9.2
+},
+{
+    id: 8,
+    leto: 2007,
+    lokalno_ogrevanje: 11.8,
+    centralno_ogrevanje: 79.5,
+    daljinsko_ogrevanje: 8.6
+},
+{
+    id: 9,
+    leto: 2008,
+    lokalno_ogrevanje: 9.8,
+    centralno_ogrevanje: 81,
+    daljinsko_ogrevanje: 9.2
+},
+{
+    id: 10,
+    leto: 2009,
+    lokalno_ogrevanje: 10,
+    centralno_ogrevanje: 80.6,
+    daljinsko_ogrevanje: 9.4
+},
+{
+    id: 11,
+    leto: 2010,
+    lokalno_ogrevanje: 10.4,
+    centralno_ogrevanje: 80.1,
+    daljinsko_ogrevanje: 9.6
+},
+{
+    id: 12,
+    leto: 2011,
+    lokalno_ogrevanje: 10.2,
+    centralno_ogrevanje: 80.9,
+    daljinsko_ogrevanje: 8.9
+},
+{
+    id: 13,
+    leto: 2012,
+    lokalno_ogrevanje: 10.3,
+    centralno_ogrevanje: 81.1,
+    daljinsko_ogrevanje: 8.6
+},
+{
+    id: 14,
+    leto: 2013,
+    lokalno_ogrevanje: 10.7,
+    centralno_ogrevanje: 80.8,
+    daljinsko_ogrevanje: 8.6
+},
+{
+    id: 15,
+    leto: 2014,
+    lokalno_ogrevanje: 13.5,
+    centralno_ogrevanje: 78.2,
+    daljinsko_ogrevanje: 8.3
+},
+{
+    id: 16,
+    leto: 2015,
+    lokalno_ogrevanje: 11.2,
+    centralno_ogrevanje: 79.9,
+    daljinsko_ogrevanje: 8.9
+},
+{
+    id: 17,
+    leto: 2016,
+    lokalno_ogrevanje: 10.7,
+    centralno_ogrevanje: 80.1,
+    daljinsko_ogrevanje: 9.2
+},
+{
+    id: 18,
+    leto: 2017,
+    lokalno_ogrevanje: 10.8,
+    centralno_ogrevanje: 80,
+    daljinsko_ogrevanje: 9.2
+},
+{
+    id: 19,
+    leto: 2018,
+    lokalno_ogrevanje: 10.3,
+    centralno_ogrevanje: 80.2,
+    daljinsko_ogrevanje: 9.5
+},
+{
+    id: 20,
+    leto: 2019,
+    lokalno_ogrevanje: 13.4,
+    centralno_ogrevanje: 77.4,
+    daljinsko_ogrevanje: 9.2
+},
+{
+    id: 21,
+    leto: 2020,
+    lokalno_ogrevanje: 13.8,
+    centralno_ogrevanje: 76.8,
+    daljinsko_ogrevanje: 9.4
+},
 ];
