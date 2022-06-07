662d39c49963c3693b6173a284c96eaf1e55c467
Doca123 <marko.mladenovic1@student.um.si>
2022-06-07 19:39:37 +0200

    Update GorivoCestni.js

--- src/components/GorivoCestni.js
+++ src/components/GorivoCestni.js
@@ -1,35 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
-import PieChart from "./PieChart";
+import LineChart from "./LineChart";
 import { gorivoCestniPrevoz } from "../Data";
 
 function GorivoCestni() {
 
-  const [GorivoCestniList, setGorivoCestniList] = useState({
-    labels: gorivoCestniPrevoz.map((data) => data.leto),
+  const [GorivoCestniList, setGorivoCestniList] = useState([]);
+
+
+
+
+  const fetchData = () => {
+    Axios.get("http://localhost:3001/stats5").then((response) => {
+      setGorivoCestniList(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+    
+  }, []);
+
+
+
+  const data1={
+    labels: GorivoCestniList.map((data) => data.leto),
     datasets: [
     {
-      label: "Gorivo na 1000km",
-      data: gorivoCestniPrevoz.map((data) => data.gorivo),
+      label: "Lokalno Ogrevanje",
+      data: GorivoCestniList.map((data) => data.gorivo),
       backgroundColor: [
-        "rgba(75,192,192,1)",
-        "#ecf0f1",
-        "#f3ba2f",
-        "#2a71d0",
+        "rgb(153, 255, 102)"
+        
       ],
       borderColor: "black",
       borderWidth: 2,
     },
     ],
-  });
+  };
 
   return (
     <div className="App">
-              <div style={{ width: 650 }}>
-              <PieChart chartData={GorivoCestniList} />
- 
-      </div>
-    </div>
+              <div>
+                <LineChart chartData={data1} />
+              </div>
+            </div>
   );
 }
 
