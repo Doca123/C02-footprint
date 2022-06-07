79cf2bba7319477b2cbfbe8aab5e3f9554e9302a
Doca123 <marko.mladenovic1@student.um.si>
2022-06-07 19:46:07 +0200

    update

--- src/components/GorivoMednarodni.js
+++ src/components/GorivoMednarodni.js
@@ -1,37 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
-import PieChart from "./PieChart";
+import BarChart from "./BarChart";
 import { gorivoMednarodniPrevoz } from "../Data";
 
 function GorivoMednarodni() {
 
-    const [GorivoMednarodniList, setGorivoMednarodniList] = useState({
-      labels: gorivoMednarodniPrevoz.map((data) => data.leto),
-      datasets: [
-      {
-        label: "Gorivo na 1000km",
-        data: gorivoMednarodniPrevoz.map((data) => data.gorivo),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-      ],
-    });
+  const [GorivoMednarodniList, setGorivoMednarodniList] = useState([]);
 
+
+
+
+  const fetchData = () => {
+    Axios.get("http://localhost:3001/stats9").then((response) => {
+      setGorivoMednarodniList(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+    
+  }, []);
+
+
+
+  const data1={
+    labels: GorivoMednarodniList.map((data) => data.leto),
+    datasets: [
+    {
+      label: "Lokalno Ogrevanje",
+      data: GorivoMednarodniList.map((data) => data.gorivo),
+      backgroundColor: [
+        "rgb(153, 255, 102)"
+        
+      ],
+      borderColor: "black",
+      borderWidth: 2,
+    },
+    ],
+  };
 
   return (
     <div className="App">
-              <div style={{ width: 650 }}>
-              <PieChart chartData={GorivoMednarodniList} />
-
-      </div>
-    </div>
+              <div>
+                <BarChart chartData={data1} />
+              </div>
+            </div>
   );
 }
-
 export default GorivoMednarodni;
