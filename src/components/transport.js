e5a04871ab911d3eceb09462f3344794798e024d
viktornankovski <viktor.nankovski@student.um.si>
2022-06-07 21:34:23 +0200

    update transport.js

--- src/components/transport.js
+++ src/components/transport.js
@@ -1,10 +1,27 @@
 import { transportation } from "../Data";
 import PieChart from "./PieChart";
 
+const transport={
+    labels: transportation.map((data) => data.type),
+    datasets: [
+    {
+      label: "Percent",
+      data: transportation.map((data) => data.percent),
+      backgroundColor: [
+        "rgb(153, 255, 102)"
+        
+      ],
+      borderColor: "black",
+      borderWidth: 2,
+    }
+    ],
+  };
+
 const Transport = () => {
     return (
-        <div><h2 className="subtitle">Average transport CO2 footprint</h2>
+        <div style={{ width: 500 }}><h2 className="subtitle">Average transport CO2 footprint</h2>
         <p className="info">Slovenia CO2 Emissions for transport is -- 5 033 642 tons.</p>
+        <PieChart chartData={transport} />
         </div>
     )
     
