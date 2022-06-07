75d9a73e862b6157b216048f9a2280d3547032b2
viktornankovski <viktor.nankovski@student.um.si>
2022-06-07 20:54:38 +0200

    update buildings.js

--- src/components/buildings.js
+++ src/components/buildings.js
@@ -1,7 +1,8 @@
 const Buildings = () => {
     return (
         <div><h2 className="subtitle">Average buildings CO2 footprint</h2>
-        <p className="info">Slovenia CO2 Emissions for buildings is -- 2 142 630 tons.</p>
+        <p className="info">The built environment generates nearly 50% of annual global CO2 emissions.
+Of those total emissions, building operations are responsible for 27% annually, while building materials and construction (typically referred to as embodied carbon) are responsible for an additional 20% annually. Slovenia CO2 Emissions for buildings is -- 2 142 630 tons.</p>
         </div>
     )
     
