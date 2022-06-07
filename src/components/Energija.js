8dee649084900850d8f575785179cad1f04f0f52
Doca123 <marko.mladenovic1@student.um.si>
2022-06-07 19:22:39 +0200

    Update Energija.js

--- src/components/Energija.js
+++ src/components/Energija.js
@@ -1,32 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
 import LineChart from "./LineChart";
 import { energijaLetno } from "../Data";
 
 function Energija() {
   
-  const [energijaList, setEnergijaList] = useState({
-    labels: energijaLetno.map((data) => data.leto),
+  const [EnergijaList, setEnergijaList] = useState([]);
+
+
+
+
+  const fetchData = () => {
+    Axios.get("http://localhost:3001/stats").then((response) => {
+        setEnergijaList(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+    
+  }, []);
+
+
+
+  const data1={
+    labels: EnergijaList.map((data) => data.leto),
     datasets: [
     {
-      label: "Proizvodnja",
-      data: energijaLetno.map((data) => data.proizvodnja),
+      label: "Lokalno Ogrevanje",
+      data: EnergijaList.map((data) => data.proizvodnja),
       backgroundColor: [
-        "rgba(75,192,192,1)"
+        "rgb(153, 255, 102)"
         
       ],
       borderColor: "black",
       borderWidth: 2,
     },
     ],
-  });
+  };
 
   return (
     <div className="App">
               <div>
-              <LineChart chartData={energijaList} />
-      </div>
-    </div>
+                <LineChart chartData={data1} />
+              </div>
+            </div>
   );
 }
 
